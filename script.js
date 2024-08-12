cee0c685f31c40feaca258ce642a7e11cd8502a2
Barrak.samer <brrak41@gmail.com>
2024-08-12 15:09:31 +0200

    Update script.js to fetch the correct sender ip address

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function retrieveLogsFromLocalStorage() {
 // Function to get the visitor's real IP address
 async function getVisitorIP() {
   try {
-    const response = await fetch('https://api.ipify.org?format=json');
+    const response = await fetch('https://api.ipify.org/?format=json');
     const data = await response.json();
     return data.ip;
   } catch (error) {
